b49bfefb089b3983dff4c2ec918cb8b8d25a2955
jailan <jailanmedhatt2@hmail.com>
2024-02-20 08:30:34 +0200

    re

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         initialRoute: LoginPage.routeName,
         routes: {
-          'HomeScreen' :(context) => HomePage(),
+          // 'HomeScreen' :(context) => HomePage(),
           'Schedule' : (context) => SchadualePgae(),
           'Tips' : (context) => TipsPage(),
           'SlideMenu' :(context) => SlideMenu(),
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: HomePage(),
+
       ),
     );
   }
