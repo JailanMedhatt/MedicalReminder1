0a282d4b943d7cddbdf26dc7b6334450a3206e48
dtare <dtarek153gmail.com>
2024-02-21 22:57:34 +0200

    EDIT MEDICINE 2.

--- lib/CustomWidgets/check MedType.dart
+++ lib/CustomWidgets/check MedType.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
 
-class CheckMedType extends StatelessWidget {
+class CheckMedType extends StatefulWidget {
   String type;
   String IMG;
+  bool? iconIsPressed;
 
-  CheckMedType({required this.type, required this.IMG});
+  CheckMedType({required this.type, required this.IMG, this.iconIsPressed});
 
   @override
+  State<CheckMedType> createState() => _CheckMedTypeState();
+}
+
+class _CheckMedTypeState extends State<CheckMedType> {
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        // Icon(
-        // Icons.check_circle,
-        // size: 24.0,color:Color(0xff5D65B0) ,
-        // ),
-        Icon(
-          Icons.circle_outlined,
-          size: 24.0,
-          color: Color(0xff5D65B0),
-        ),
-        Text(
-          type,
-          style: TextStyle(
-              fontSize: 22,
+    return Flexible(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Icon(
+          // Icons.check_circle,
+          // size: 24.0,color:Color(0xff5D65B0) ,
+          // ),
+          IconButton(
+            icon: Icon(
+              widget.iconIsPressed == true
+                  ? Icons.check_circle
+                  : Icons.circle_outlined,
+              size: 24.0,
               color: Color(0xff5D65B0),
-              fontWeight: FontWeight.w600),
-        ),
-        Image.asset(
-          IMG,
-          width: 38,
-          height: 37,
-        )
-      ],
+            ),
+            tooltip: 'Choose Medicine Type',
+            onPressed: () {
+              setState(() {
+                widget.iconIsPressed = true;
+              });
+            },
+          ),
+          Text(
+            widget.type,
+            style: TextStyle(
+                fontSize: 22,
+                color: Color(0xff5D65B0),
+                fontWeight: FontWeight.w600),
+          ),
+          Image.asset(
+            widget.IMG,
+            width: 38,
+            height: 37,
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/CustomWidgets/custom EditDateRow.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateEditRow extends StatefulWidget {
+  String title;
+  String date;
+
+  DateEditRow({required this.title, required this.date});
+
+  @override
+  State<DateEditRow> createState() => _DateEditRowState();
+}
+
+class _DateEditRowState extends State<DateEditRow> {
+  String selectedDate = DateFormat("dd-MM-yyyy").format(DateTime.now());
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                widget.title,
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Color(0xff5D65B0),
+                    fontWeight: FontWeight.w600),
+              ),
+              Icon(
+                Icons.date_range_outlined,
+                color: Color(0xff5D65B0),
+              ),
+            ],
+          ),
+          InkWell(
+            onTap: () {
+              showCalendar();
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xEDF2F3).withOpacity(0.99),
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 4,
+                    offset: Offset(4, 4), // Shadow position
+                  ),
+                ],
+              ),
+              //Type TextField
+              width: 130,
+              height: 40,
+              child: Text(
+                textAlign: TextAlign.center,
+                '${widget.date}', // pass the hint text parameter here
+                style: TextStyle(
+                    color: Color(0x887E7E).withOpacity(0.99), fontSize: 21),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showCalendar() {
+    var chosenDate = showDatePicker(
+        context: context,
+        barrierDismissible: false,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime.now().add(Duration(days: 365)));
+    if (chosenDate != null) {
+      selectedDate = chosenDate as String;
+      setState(() {});
+    }
+  }
+}
